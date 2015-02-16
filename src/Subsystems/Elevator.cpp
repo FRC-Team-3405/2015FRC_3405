d6a687003566f1154f157df6f2df3fb30ca28b7f
Ty Sweat <tysweat0@gmail.com>
2015-02-16 08:15:59 -0700

    Added function to get encoder value to Elevator subsystem

--- src/Subsystems/Elevator.cpp
+++ src/Subsystems/Elevator.cpp
@@ -4,15 +4,25 @@
 Elevator::Elevator() :
 		Subsystem("Elevator")
 {
-
+	enc_1 = new Encoder(0,1);
+	enc_2 = new Encoder(2,3);
 }
 
 void Elevator::InitDefaultCommand()
 {
 	// Set the default command for a subsystem here.
 	//SetDefaultCommand(new MySpecialCommand());
+	SetDefaultCommand(new LogEncoders());
 }
 
 // Put methods for controlling this subsystem
 // here. Call these from Commands.
-
+int Elevator::GetEncoder(int e = 1)
+{
+	if(e == 1)
+		return enc_1->Get();
+	else if(e == 2)
+		return enc_2->Get();
+	else
+		return -1;
+}
--- src/Subsystems/Elevator.h
+++ src/Subsystems/Elevator.h
@@ -4,14 +4,19 @@
 #include "Commands/Subsystem.h"
 #include "WPILib.h"
 
+#include "../Commands/LogEncoders.h"
+
 class Elevator: public Subsystem
 {
 private:
 	// It's desirable that everything possible under private except
 	// for methods that implement subsystem capabilities
+	Encoder* enc_1;
+	Encoder* enc_2;
 public:
 	Elevator();
 	void InitDefaultCommand();
+	int GetEncoder(int);
 };
 
 #endif
