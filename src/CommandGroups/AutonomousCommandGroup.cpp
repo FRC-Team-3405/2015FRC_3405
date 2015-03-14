44f526a51a555a6f9b46a5380d5567ba739eb0bc
Ty Sweat <tysweat0@gmail.com>
2015-03-14 16:08:17 -0600

    Final competition code. So bittersweet.

--- src/CommandGroups/AutonomousCommandGroup.cpp
+++ src/CommandGroups/AutonomousCommandGroup.cpp
@@ -5,8 +5,8 @@ const float TIME_TO_BACK = .5;
 AutonomousCommandGroup::AutonomousCommandGroup()
 {
 	//Clamp
-	AddSequential(new ClampArm());
+	//AddSequential(new ClampArm());
 	//Drive forward for 1 second
-	AddSequential(new DriveDirectionalCommand(0, -1, 0), 2.5);
+	//AddSequential(new DriveDirectionalCommand(0, -1, 0), 2.5);
 
 }
